3db28770951ba2a4cc0ab5654e09010b77efd3c4
genni <genni@LAPTOP-6M551750>
2025-05-11 01:43:53 +0300

    Fixed using in tests

--- TaskManagement/Tests/GlobalUsing.cs
+++ TaskManagement/Tests/GlobalUsing.cs
@@ -7,21 +7,18 @@ global using Microsoft.Extensions.DependencyInjection.Extensions;
 global using Microsoft.Extensions.Configuration;
 global using System.Collections.Generic;
 global using System.Linq;
-global using TaskManagement.Infrastructure.Data; // Reference Infrastructure DbContext
-global using TaskManagement.Domain.Models; // Reference Domain models
-global using Testcontainers.PostgreSql; // Testcontainers for PostgreSQL
+global using Testcontainers.PostgreSql; 
 global using System.Threading.Tasks;
 global using System;
-global using Microsoft.IdentityModel.Tokens; // Needed for JwtBearer
-global using Microsoft.AspNetCore.Authentication; // Needed for AuthenticationBuilder
-global using System.Security.Claims; // Needed for ClaimsIdentity
-global using Microsoft.AspNetCore.Authentication.JwtBearer; // Needed for JwtBearerDefaults
-global using AutoMapper; // Needed for AutoMapper setup (Still needed if other parts use it)
-global using Respawn; // Added for Respawn
-global using Npgsql; // Needed for NpgsqlConnection
-global using Microsoft.Extensions.Logging; // Needed for logging in factory
-global using Microsoft.Extensions.Options; // Needed for IOptions
-global using TaskManagement.Application.Options; // Reference the Options classes
+global using Microsoft.IdentityModel.Tokens; 
+global using Microsoft.AspNetCore.Authentication; 
+global using System.Security.Claims;
+global using Microsoft.AspNetCore.Authentication.JwtBearer; 
+global using AutoMapper; 
+global using Respawn; 
+global using Npgsql; 
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Options;
 global using Microsoft.Extensions.Caching.Distributed; 
 global using Testcontainers.Redis; 
 global using Moq;
