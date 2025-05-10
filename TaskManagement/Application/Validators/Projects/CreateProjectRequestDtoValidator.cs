773be72cc844a603087f1380cef7e8a575b329f4
genni <genni@LAPTOP-6M551750>
2025-05-10 23:30:37 +0300

    Added Validator

---
+++ TaskManagement/Application/Validators/Projects/CreateProjectRequestDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using Application.DTOs.Projects;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validators.Projects
+{
+    public class CreateProjectRequestDtoValidator : AbstractValidator<CreateProjectRequestDto>
+    {
+        public CreateProjectRequestDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Project name is required.")
+                .MaximumLength(100).WithMessage("Project name cannot exceed 100 characters.");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Project description cannot exceed 500 characters.");
+        }
+    }
+}
---
+++ TaskManagement/Application/Validators/Projects/CreateTaskRequestDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTOs.Tasks;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validators.Projects
+{
+    public class CreateTaskRequestDtoValidator : AbstractValidator<CreateTaskRequestDto>
+    {
+        public CreateTaskRequestDtoValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Task title is required.")
+                .MaximumLength(100).WithMessage("Task title cannot exceed 100 characters.");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Task description cannot exceed 500 characters.");
+
+            RuleFor(x => x.Status)
+                .IsInEnum().WithMessage("Invalid task status.");
+        }
+    }
+}
